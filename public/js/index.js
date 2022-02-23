4a10a60ec2c1194b19e7185cbe08e931eafc2236
Gabriel Warren <gwarren@andrew.cmu.edu>
2022-02-23 18:19:30 -0500

    styled alerts

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,16 @@
 const spotifyEmbed = document.getElementById("spotify-embed");
+const correctAlert = document.getElementById("correct");
+const wrongAlert = document.getElementById("incorrect");
+const incorrectBtnClose = document.getElementById("incorrectBtnClose");
+const correctBtnClose = document.getElementById("correctBtnClose");
 const nextBtn = document.getElementById("next");
 const homeBtn = document.getElementById("home");
-const correctAnswer = document.getElementById("songTitle").innerText;
 const choiceBtn = document.getElementById("choiceBtn");
 const choiceBtn2 = document.getElementById("choiceBtn2");
 const choiceBtn3 = document.getElementById("choiceBtn3");
 const choiceBtn4 = document.getElementById("choiceBtn4");
+
+const correctAnswer = document.getElementById("songTitle").innerText;
 
 const choiceBtns = [choiceBtn, choiceBtn2, choiceBtn3, choiceBtn4];
 
@@ -21,14 +26,22 @@ homeBtn.addEventListener("click", () => {
   window.location.href = "http://localhost:8080";
 });
 
+incorrectBtnClose.addEventListener("click", () => {
+  wrongAlert.style.display = "none";
+});
+
+correctBtnClose.addEventListener("click", () => {
+  correctAlert.style.display = "none";
+});
+
 for (let index = 0; index < choiceBtns.length; index++) {
   choiceBtns[index].addEventListener("click", () => {
     const text = choiceBtns[index].innerText;
     if (text === correctAnswer) {
-      alert("Correct!");
+      correctAlert.style.display = "block";
       window.location.href = "http://localhost:8080/next";
     } else {
-      alert("Wrong! It was " + correctAnswer);
+      wrongAlert.style.display = "block";
       window.location.href = "http://localhost:8080/next";
     }
   });
